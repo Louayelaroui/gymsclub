97cfb37e7667b6a2cc4f142af1c70b1a523dd98b
Ahmed Ch <ahmedchouihi@gmail.com>
2022-07-30 11:44:25 +0100

    addFooter

--- wp-content/themes/gymsclub/footer.php
+++ wp-content/themes/gymsclub/footer.php
@@ -11,21 +11,54 @@
 
 ?>
 
-	<footer id="colophon" class="site-footer">
-		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'gymsclub' ) ); ?>">
-				<?php
-				/* translators: %s: CMS name, i.e. WordPress. */
-				printf( esc_html__( 'Proudly powered by %s', 'gymsclub' ), 'WordPress' );
-				?>
-			</a>
-			<span class="sep"> | </span>
-				<?php
-				/* translators: 1: Theme name, 2: Theme author. */
-				printf( esc_html__( 'Theme: %1$s by %2$s.', 'gymsclub' ), 'gymsclub', '<a href="http://underscores.me/">Underscores.me</a>' );
-				?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+	<footer>
+        <div class="container grid">
+            <div class="box">
+                <img src="images/logo2.png" alt="GymsClub">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis commodi                          consectetur dolores ex facere fuga illum ipsum, laudantium odit, quam sed tempore,
+                    tenetur vitae voluptas voluptate! Accusantium dolor eum hic?
+                </p>
+                <div class="icon">
+                    <i class="fa fa-facebook-f"></i>
+                    <i class="fa fa-instagram"></i>
+                    <i class="fa fa-twitter"></i>
+                    <i class="fa fa-youtube"></i>
+                </div>
+            </div>
+
+            <div class="box">
+                <h2>Links</h2>
+                <ul>
+                    <li>Company History</li>
+                    <li>About Us</li>
+                    <li>Contact Us</li>
+                    <li>Services</li>
+                    <li>Privacy Policy</li>
+                </ul>
+            </div>
+
+            <div class="box">
+                <h2>Contact us</h2>
+                <ul>
+                <li><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam, esse.</p></li>
+
+                <li><i class="fa fa-location-arrow"></i>
+                <label for="">15554 park street Sousse;Tunisia</label></li>
+                <li><i class="fa fa-phone"></i>
+                    <label for="">88 454 484 484</label></li>
+                <li><i class="fa fa-envelope"></i>
+                <label for="">contact@gymsclub.com</label></div></li>
+            </ul>
+            </div>
+        </div>
+    </footer>
+    <div class="legal">
+        <p class="container">
+            Copyright 2022 All rights are reserved
+        </p>
+    </div>
+
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
